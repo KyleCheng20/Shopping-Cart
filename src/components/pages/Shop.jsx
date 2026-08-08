7816b596345200d41b1f85cae1fdc5ffffbdf078
Kyle Cheng <kcheng120@gmail.com>
2026-08-08 19:29:44 -0400

    Fetch from api and render 1 product onto shop screen

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,7 +1,48 @@
+import { useState ,useEffect } from "react";
+
 export default function Shop() {
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchProducts() {
+            try {
+                const response = await fetch("https://dummyjson.com/products?limit=0");
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error: Status ${response.status}`);
+                }
+
+                const data = await response.json();
+                const productsData = data.products;
+                setProducts(productsData);
+                setError(null);
+            } catch (e) {
+                setError(e.message);
+                setProducts([]);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchProducts();
+    }, []);
+
     return (
         <main>
-            <h1>Shop</h1>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <img src={products[97].images[0]} alt={products.title} />
+                    <h3>{products[97].title}</h3>
+                    <p>Price: ${products[97].price}</p>
+                    <p>Rating: {products[97].rating}/5</p>
+                    <p>Stock: {products[97].stock}</p>
+                    <p>Status: {products[97].availabilityStatus}</p>
+                </>
+            )}
         </main>
     )
 }
---
+++ src/components/ui/ProductCard.jsx
@@ -0,0 +1,5 @@
+export default function ProductCard({ title, price, rating, stock, status }) {
+    <div className="product-card">
+
+    </div>
+}
